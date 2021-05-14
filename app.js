24fed769b57b80dfeb760906c725f75318fbf41a
Gamid <gamaizingg@gmail.com>
2021-05-14 04:20:17 +0300

    Change authentication login pass. Second

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ const transporter = nodemailer.createTransport({
   port: 465,
   secure: true,
   auth: {
-    user: "juliana.priroda@gmail.com",
-    pass: "44L-fdw-7vX-rEZ"
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
   }
 });
 
