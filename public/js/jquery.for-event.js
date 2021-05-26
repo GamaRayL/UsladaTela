f7f7f069aa403ebb9e521a658ef12b92bf6c2f75
Gamid <gamaizingg@gmail.com>
2021-05-26 11:56:38 +0300

    Added: burger-menu, space between li .mentor and fix navigation who wrap over section when theme choosing.

--- public/js/jquery.for-event.js
+++ public/js/jquery.for-event.js
@@ -1,30 +1,58 @@
 $(function () {
+  $(".navigation-menu__burger").on("click", function () {
+    $(".navigation-menu__burger").toggleClass("open-menu");
+    $(".navigation-list").toggleClass("open-menu");
+  });
+
+  $(".link-decoration").on("click", function () {
+    $(".navigation-menu__burger").toggleClass("open-menu");
+    $(".navigation-list").toggleClass("open-menu");
+  });
+
+  $(".content").on("click", function () {
+    $(".navigation-menu__burger").toggleClass("open-menu");
+    $(".navigation-list").toggleClass("open-menu");
+  });
 
   // Редактирует введённый код под заданный формат. Jquery плагин.
 
   $("#phone").mask("+7 (999) 999-99-99");
 
-
   // Добавление свойств по наведение на одни элементы, что задействуют другие.
 
   $(".detox-main").on("mouseover", function () {
-    $(".leaves-graph").css("filter", "drop-shadow(0px 0px 10px rgba(129, 178, 20, 1))");
+    $(".leaves-graph").css(
+      "filter",
+      "drop-shadow(0px 0px 10px rgba(129, 178, 20, 1))"
+    );
   });
 
   $(".detox-main").on("mouseout", function () {
     $(".leaves-graph").css("filter", "none");
   });
 
-  $(".currently-programme-top-main-description-list").on("mouseover", function () {
-    $(".body-graph").css("filter", "drop-shadow(0px 0px 25px rgba(255, 204, 41, 1))");
-  });
+  $(".currently-programme-top-main-description-list").on(
+    "mouseover",
+    function () {
+      $(".body-graph").css(
+        "filter",
+        "drop-shadow(0px 0px 25px rgba(255, 204, 41, 1))"
+      );
+    }
+  );
 
-  $(".currently-programme-top-main-description-list").on("mouseout", function () {
-    $(".body-graph").css("filter", "none");
-  });
+  $(".currently-programme-top-main-description-list").on(
+    "mouseout",
+    function () {
+      $(".body-graph").css("filter", "none");
+    }
+  );
 
   $(".currently-programme-top-main-human").on("mouseover", function () {
-    $(".body-graph").css("filter", "drop-shadow(0px 0px 25px rgba(255, 204, 41, 1))");
+    $(".body-graph").css(
+      "filter",
+      "drop-shadow(0px 0px 25px rgba(255, 204, 41, 1))"
+    );
   });
 
   $(".currently-programme-top-main-human").on("mouseout", function () {
@@ -32,19 +60,23 @@ $(function () {
   });
 
   $(".transformation-game-description").on("mouseover", function () {
-    $(".cube-graph").css("filter", "drop-shadow(0px 0px 25px rgba(81, 18, 129, 1))");
+    $(".cube-graph").css(
+      "filter",
+      "drop-shadow(0px 0px 25px rgba(81, 18, 129, 1))"
+    );
   });
 
   $(".transformation-game-description").on("mouseout", function () {
     $(".cube-graph").css("filter", "none");
   });
 
-
   // _________________________
 
-
   $(".transformation-game-cube").on("mouseover", function () {
-    $(".cube-graph").css("filter", "drop-shadow(0px 0px 25px rgba(81, 18, 129, 1))");
+    $(".cube-graph").css(
+      "filter",
+      "drop-shadow(0px 0px 25px rgba(81, 18, 129, 1))"
+    );
   });
 
   $(".transformation-game-cube").on("mouseout", function () {
